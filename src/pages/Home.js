0507354fd33ede106e08f36e2d242e1fdaa5e816
stanleywanjau <stanleywanjau371@gmail.com>
2023-11-21 13:04:24 +0300

    passed props to children

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import { useState,useEffect } from "react"
+import Trousers from "./Trousers"
+import Shirts from "./Shirts"
 function Home(){
-  const[isporduct,setIsPorduct]=useState([])
-  useEffect(()=>{})
+  const[isproduct,setIsProduct]=useState([])
+  useEffect(()=>{
+    fetch(" http://localhost:3000/products")
+    .then(res=>res.json())
+    .then((products)=>setIsProduct(products))
+  },[])
   return (
+    <>
     <h1>HOME</h1>
+    <Shirts isproduct={isproduct}/>
+    <Trousers isproduct={isproduct}/>
+    </>
   )
 }
 export default Home
