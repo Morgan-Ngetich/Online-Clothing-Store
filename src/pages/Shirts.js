b8c2acf6fcb402eedea1b8b15868ae9569f0770e
TyreseMugoNyaga <tymugo@gmail.com>
2023-11-21 21:42:34 -0800

    shirtspage

--- src/pages/Shirts.js
+++ src/pages/Shirts.js
@@ -1,7 +1,31 @@
-import React from 'react'
+// import React, { useState, useEffect } from 'react';
 
-export default function Shirts() {
+function Shirts({isproduct}) {
+  
+
+ 
+  
   return (
-    <div>Shirts</div>
-  )
+    <div className="card-container">
+     
+      {/* Render the fetched shirt data here */}
+      {isproduct?.shirts?.map(shirt => (
+        <div className= "card" key={shirt.id}>
+          <img className="card-image" src={shirt.image} alt= {shirt.name}/>
+          <div className="card-details">
+          <p> {shirt.name}</p>
+          <p><span>Brand:</span> {shirt.brand}</p>
+          <p><span>Price:</span> {shirt.price}</p>
+          <p><span>Size:</span> {shirt.size}</p>
+          <button className="add-to-cart">Add To Cart</button>
+          </div>
+          
+       
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export default Shirts;
+
