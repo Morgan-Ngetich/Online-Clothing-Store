647b3cf43112e08d4b9ac01f8919e6cf4a71ca2e
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-11-24 12:23:19 +0300

    Changes of trousers

--- src/pages/Trousers.js
+++ src/pages/Trousers.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React ,{useState}from 'react';
+
 
 function Trousers({ isproduct, onAddToCart }) {
-  //console.log('Trousers', isproduct.trousers.stock); 
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleAddToCart = (product) => {
+    // Toggle the inCart status when adding to the cart
+    product.inCart = !product.inCart;
+    onAddToCart(product);
+  };
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredTrousers =
+    searchTerm.length > 3
+      ? isproduct?.trousers?.filter((trouser) =>
+          trouser.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : isproduct?.trousers;
   return (
+    <>
+    <div>
+        <input
+          type="text"
+          placeholder="Search trouser..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
     <div className="card-container">
-      {isproduct?.trousers?.map((product) => (
+      {filteredTrousers?.map((product) => (
         <div key={product.id} className="card">
           <img src={product.image} alt={product.name} className="card-image" />
           <div className="card-details">
@@ -13,12 +38,20 @@ function Trousers({ isproduct, onAddToCart }) {
             <p><span>Price:</span> {product.price}</p>
             <p><span>Size:</span> {product.size}</p>
             <p><span>Stock:</span> {product.stock}</p>
-            <button className='add-to-cart' onClick={() => onAddToCart(product)}>Add to cart</button>
+            {/* Wrap the card details in a Link to the cart */}
+         
+            <button
+              className="add-to-cart"
+              onClick={() => handleAddToCart(product)}
+            >
+              {product.inCart ? 'In Cart' : 'Add to Cart'}
+            </button>
+           
           </div>
         </div>
       ))}
     </div>
-  
+    </>
   );
 }
 
